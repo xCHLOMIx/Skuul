b2e4befba6e479c4381b899a4f42aa5bca86b9fd
xCHLOMIx <gutabarwaa@gmail.com>
2025-04-03 02:34:32 +0200

    ADDED: Image on Admin Login Form

--- client/src/pages/admin/AdminLogin.tsx
+++ client/src/pages/admin/AdminLogin.tsx
@@ -1,14 +1,17 @@
 import React from 'react'
 import PrimaryButton from '../../components/universal/PrimaryButton'
 import { GoShieldLock } from "react-icons/go";
+import image from "../../assets/login-bg.svg"
 
 const AdminLogin: React.FC = () => {
     return (
         <div className='h-full w-full flex overflow-hidden'>
-            <div className='h-full max-lg:hidden min-w-2/3 bg-primary'></div>
+            <div className='h-full max-lg:hidden min-w-2/3 flex justify-center items-center bg-primary'>
+                <img src={image} className='w-3/4 opacity-90' alt="" />
+            </div>
             <div className='h-full w-full px-12 flex flex-col justify-center gap-4'>
                 <div className='flex items-center gap-3'>
-                    <GoShieldLock color='#D55A29' size={32}/>
+                    <GoShieldLock color='#D55A29' size={32} />
                     <h1 className='text-2xl font-bold'>Admin Login</h1>
                 </div>
                 <form className='w-full flex flex-col gap-4'>
@@ -28,7 +31,7 @@ const AdminLogin: React.FC = () => {
                             className='p-3.5 border border-gray-300 outline-0'
                         />
                     </div>
-                    <PrimaryButton styles='' text='Log in' />
+                    <PrimaryButton styles='hover:bg-white hover:text-primary border-2 transition duration-200 border-primary hover:border-primary' text='Log in' />
                 </form>
             </div>
         </div>
