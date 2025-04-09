2278e5cd8eab5ec3df63d41b0847820c27d8f090
xCHLOMIx <gutabarwaa@gmail.com>
2025-04-09 21:20:08 +0200

    CHANGED: NotificationContext.tsx to AlertContext.tsx

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -3,31 +3,34 @@ import AdminLayout from "./layouts/AdminLayout"
 import AdminSignin from "./pages/admin/AdminSignin"
 import StudentSignin from "./pages/student/StudentSignin"
 import StudentSignup from "./pages/student/StudentSignup"
+import { AlertContextProvider } from "./context/AlertContext"
 
 function App() {
 
   return (
     <div className="h-screen">
-      <BrowserRouter>
-        <Routes>
-          <Route
-            path="/admin/*"
-            element={<AdminLayout />}
-          />
-          <Route
-            path='/admin/signin'
-            element={<AdminSignin />}
-          />
-          <Route
-            path='/student/signin'
-            element={<StudentSignin />}
-          />
-          <Route
-            path='/student/signup'
-            element={<StudentSignup />}
-          />
-        </Routes>
-      </BrowserRouter>
+      <AlertContextProvider>
+        <BrowserRouter>
+          <Routes>
+            <Route
+              path="/admin/*"
+              element={<AdminLayout />}
+            />
+            <Route
+              path='/admin/signin'
+              element={<AdminSignin />}
+            />
+            <Route
+              path='/student/signin'
+              element={<StudentSignin />}
+            />
+            <Route
+              path='/student/signup'
+              element={<StudentSignup />}
+            />
+          </Routes>
+        </BrowserRouter>
+      </AlertContextProvider>
     </div>
   )
 }
